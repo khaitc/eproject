04fedd5995142cc74f3a738a111673d9007d797a
Quân Trần <einstein9x961@gmail.com>
2018-05-25 08:45:16 +0700

    bao nop cho nha truong

---
+++ Quan/E-project CARVAN/js/product.js
@@ -0,0 +1,41 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+
+    var dots = document.getElementsByClassName("demo");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    var index = slideIndex - 1;
+    slides[index].style.display = "block";
+    dots[index].className += " active";
+}
+
+// if ($("#product-home").hasClass('hide')) {
+//     $("#product-home").removeClass('hide');
+// }
+// function openTab(id, t) {
+//     $(document).find('[class^=tablinks]').each(function (index, value) {
+//         $(this).removeClass('active');
+//     });
+//     $(t).addClass('active');
+//     $(document).find('[id^=product]').each(function (index, value) {
+//         $(this).addClass('hide');
+//     });
+//     $(id).removeClass('hide');
+// }
